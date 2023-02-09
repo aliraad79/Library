97cde9a8fc2f09681a348fe0d450bc2647d999a6
aliraad79 <a.ahmadi.k.79@gmail.com>
2023-02-09 11:19:00 +0330

    init homepage

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+import Navbar from "../common/NavBar";
+
+function HomePage() {
+  return (
+    <Fragment>
+      <Navbar />
+      <div>
+        Test
+      </div>
+    </Fragment>
+  );
+}
+
+export default HomePage;
