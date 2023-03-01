525555ea422d3540053500180e542e63db7435e4
kianbakhtari <bakhtari.kian@gmail.com>
2023-03-01 13:31:21 +0330

    configure backend to show media

--- backend/library/urls.py
+++ backend/library/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import LibraryView
+from .views import (AttachmentView, LibraryInstanceView, LibraryView,
+                    MediaTypeView, MediaView)
 
 urlpatterns = [
     path("", LibraryView.as_view()),
+    path("<int:pk>", LibraryInstanceView.as_view()),
+    path("attachment", AttachmentView.as_view()),
+    path("media", MediaView.as_view()),
+    path("mediaTypes", MediaTypeView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
