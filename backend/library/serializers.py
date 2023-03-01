3712d341713ed8438fb3a0f84a231e317b0128d8
hamedrashidpour <Hamedrashid1421@gmail.com>
2023-03-01 13:36:28 +0330

    Add CRUD for models

--- backend/library/serializers.py
+++ backend/library/serializers.py
@@ -1,13 +1,31 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import Library
+from .models import Attachment, Library, Media, MediaType
 
 
 class LibrarySerializer(ModelSerializer):
     class Meta:
         model = Library
-        fields = ["id", "title", "description", "owner"]
+        fields = ["id", "title", "description", "owner", "accepted_file_extention"]
 
     def get_validation_exclusions(self):
         exclusions = super(LibrarySerializer, self).get_validation_exclusions()
         return exclusions + ["owner"]
+
+
+class MediaSerializer(ModelSerializer):
+    class Meta:
+        model = Media
+        fields = ["id", "data", "library"]
+
+
+class AttachmentSerializer(ModelSerializer):
+    class Meta:
+        model = Attachment
+        fields = "__all__"
+
+
+class MediaTypeSerializer(ModelSerializer):
+    class Meta:
+        model = MediaType
+        fields = "__all__"
--- backend/library/views.py
+++ backend/library/views.py
@@ -1,7 +1,11 @@
-from rest_framework import generics
+import os
 
-from .models import Library
-from .serializers import LibrarySerializer
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Attachment, Library, Media, MediaType
+from .serializers import AttachmentSerializer, LibrarySerializer, MediaSerializer
 
 
 class LibraryView(generics.ListCreateAPIView):
@@ -9,9 +13,50 @@ class LibraryView(generics.ListCreateAPIView):
     serializer_class = LibrarySerializer
 
     def post(self, request, *args, **kwargs):
+        # request.data._mutable = True
         request.data["owner"] = request.user.id
+        # request.data._mutable = False
+
         return self.create(request, *args, **kwargs)
 
     def get_queryset(self):
         user = self.request.user
         return Library.objects.filter(owner=user)
+
+
+class LibraryInstanceView(generics.RetrieveAPIView):
+    queryset = Library.objects.all()
+    serializer_class = LibrarySerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Library.objects.filter(owner=user)
+
+
+class AttachmentView(generics.CreateAPIView):
+    queryset = Attachment.objects.all()
+    serializer_class = AttachmentSerializer
+
+
+class MediaView(generics.CreateAPIView):
+    queryset = Media.objects.all()
+    serializer_class = MediaSerializer
+
+    def post(self, request, *args, **kwargs):
+        valid_extensations = Library.objects.get(
+            id=request.data["library"]
+        ).accepted_file_extention
+
+        _, file_extension = os.path.splitext(self.request.FILES.get("data").name)
+        if (
+            file_extension not in valid_extensations
+            and file_extension.replace(".", "") not in valid_extensations
+        ):
+            return Response({"Error": "Not Valid file extensation"}, status=400)
+        return self.create(request, *args, **kwargs)
+
+
+class MediaTypeView(APIView):
+    def get(self, request, format=None):
+        types = [user.name for user in MediaType.objects.all()]
+        return Response(types)
