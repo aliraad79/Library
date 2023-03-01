3590745b2a916a2907946a716a1fc160a3014f09
aliraad79 <a.ahmadi.k.79@gmail.com>
2023-03-01 13:42:36 +0330

    Add django admins

--- backend/library/admin.py
+++ backend/library/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import Library
+
+from .models import Attachment, Library, MediaType, Media
+
 
 @admin.register(Library)
 class LibraryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Attachment)
+class AttachmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MediaType)
+class MediaTypeAdmin(admin.ModelAdmin):
     pass
